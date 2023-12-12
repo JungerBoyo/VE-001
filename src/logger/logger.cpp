732e0ccbb9d6f0876e2f2b5d1e1207625a044680
JungerBoyo <BabeFromPompeii@protonmail.com>
2023-12-12 21:26:46 +0100

    fix small bug in logger.cpp

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -21,7 +21,7 @@ std::shared_ptr<spdlog::logger> ve001::logger(std::make_shared<spdlog::logger>(
     #ifndef WINDOWS
         std::make_shared<spdlog::sinks::ansicolor_stdout_sink_mt>(),
     #else
-        std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>().
+        std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>(),
     #endif
 #endif
 #if defined(VE001_LOG_TO_FILE) && !defined(WINDOWS) // for now disable on windows
